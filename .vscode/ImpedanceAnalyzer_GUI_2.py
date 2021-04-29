5383cab8793dd4646841c8a2af39f216108281c0
Lars Lindner <lindnerlars@users.noreply.github.com>
2021-04-29 06:08:54 -0700

    29/04/21

--- .vscode/ImpedanceAnalyzer_GUI_2.py
+++ .vscode/ImpedanceAnalyzer_GUI_2.py
@@ -27,8 +27,10 @@ import matplotlib.pyplot as plt
 freq_start = int(3.5e6)
 freq_end = int(4.5e6)
 freq_delta = int(10000)
+amp_start = float(0.1)
+amp_end = float(0.3)
+amp_delta = float(0.1)
 resistance = int(1000)
-amplitude = float(0.1)
 
 # Variables Definitons
 win = Tk()
@@ -49,6 +51,7 @@ else:
 
 # region Window Functions
 def connectFunction():
+    connectButton.update()
     szerr = create_string_buffer(512)
     dwf.FDwfDeviceOpen(c_int(-1), byref(hdwf))
 
@@ -66,6 +69,7 @@ def connectFunction():
 
 
 def disconnectFunction():
+    disconnectButton.update()
     dwf.FDwfDeviceClose(hdwf)
     connectButton["state"] = tk.NORMAL
     disconnectButton["state"] = tk.DISABLED
@@ -76,61 +80,75 @@ def disconnectFunction():
 
 
 def setFunction():
+    setButton.update()  # So the button 'Set Parameters' doesn't get blocked
     global freq_start
     global freq_end
     global freq_delta
     global resistance
-    global amplitude
-    freq_start = int(startInput.get())
-    freq_end = int(endInput.get())
-    freq_delta = int(deltaInput.get())
+    global amp_start
+    freq_start = int(startFreqInput.get())
+    freq_end = int(endFreqInput.get())
+    freq_delta = int(deltaFreqInput.get())
     resistance = int(resistanceInput.get())
-    amplitude = float(amplitudeInput.get())
+    amp_start = float(startAmpInput.get())
+    amp_end = float(endAmpInput.get())
+    amp_delta = float(deltaAmpInput.get())
 
     dwf.FDwfDeviceAutoConfigureSet(hdwf, c_int(3))  # this option will enable dynamic adjustment of analog out settings like: frequency, amplitude...
     dwf.FDwfAnalogImpedanceReset(hdwf)
     dwf.FDwfAnalogImpedanceModeSet(hdwf, c_int(8))  # 0 = W1-C1-DUT-C2-R-GND, 1 = W1-C1-R-C2-DUT-GND, 8 = AD IA adapter
-    dwf.FDwfAnalogImpedanceFrequencySet(hdwf, c_double(freq_start))  # frequency in Hertz
+    # dwf.FDwfAnalogImpedanceFrequencySet(hdwf, c_double(freq_start))  # frequency in Hertz
     dwf.FDwfAnalogImpedanceReferenceSet(hdwf, c_double(resistance))  # reference resistor value in Ohms
-    dwf.FDwfAnalogImpedanceAmplitudeSet(hdwf, c_double(amplitude))  # Measurement amplitude, 0V to peak signal
+    # dwf.FDwfAnalogImpedanceAmplitudeSet(hdwf, c_double(amp_start))  # Measurement amplitude, 0V to peak signal
     time.sleep(1)
 
-    infoOutput.insert(tk.INSERT, "All parameters set\n")
+    infoOutput.insert(tk.INSERT, "All Parameters set\n")
     infoOutput.see("end")
 
 
 def startFunction():
-    initial = time.time()
-    extfile = open("impedance_" + str(amplitudeInput.get()) + "V.txt", "w")
-    infoOutput.insert(tk.INSERT, "Start Measurement:" + str(amplitude) + "V \n")
+    startButton.update()
+    infoOutput.insert(tk.INSERT, "Start Measurement:" + str(amp_start) + "V \n")
+    infoOutput.update()                                 # Forces the GUI to update the text box
 
-    dwf.FDwfAnalogImpedanceConfigure(hdwf, c_int(1))  # Measurement start
+    initial = time.time()                               # Take time stamp
 
-    for hz in range(freq_start, freq_end + 1, freq_delta):
-        dwf.FDwfAnalogImpedanceFrequencySet(hdwf, c_double(hz))
-        time.sleep(0.01)
-        dwf.FDwfAnalogImpedanceStatus(hdwf, None)  # ignore last capture since we changed the frequency
+    dwf.FDwfAnalogImpedanceConfigure(hdwf, c_int(1))    # Measurement Start
 
-        while True:
-            if dwf.FDwfAnalogImpedanceStatus(hdwf, byref(sts)) == 0:
-                dwf.FDwfGetLastErrorMsg(szerr)
-                print(str(szerr.value))
-                quit()
-            if sts.value == 2:
-                break
+    for amp in range(amp_start, amp_end, amp_delta):                       # Runs all the amplitude range
 
-        dwf.FDwfAnalogImpedanceStatusMeasure(hdwf, DwfAnalogImpedanceImpedance, byref(impedance))
-        dwf.FDwfAnalogImpedanceStatusMeasure(hdwf, DwfAnalogImpedanceImpedancePhase, byref(phase))
-        extfile.write(str(hz) + "\t" + str(abs(impedance.value)) + "\t" + str((phase.value / math.pi) * 180.0) + "\n")
+        extfile = open("impedance_" + str(amp) + "V_" + str(resistance) + "Ohm.txt", "w")
+        dwf.FDwfAnalogImpedanceAmplitudeSet(hdwf, c_double(amp_start))      # Sets the stimulus amplitude (0V to peak signal)
 
-    dwf.FDwfAnalogImpedanceConfigure(hdwf, c_int(0))  # Measurement end
+        for freq in range(freq_start, freq_end + 1, freq_delta):            # Runs all the frequency range
+            dwf.FDwfAnalogImpedanceFrequencySet(hdwf, c_double(freq))       # Sets the stimulus frequency
+            time.sleep(0.01)                                                # Settle time of device under test (DUT), this value depends on the device!
+            dwf.FDwfAnalogImpedanceStatus(hdwf, None)                       # Ignore last capture since we changed the frequency
 
-    final = time.time()
+            while True:
+                if dwf.FDwfAnalogImpedanceStatus(hdwf, byref(sts)) == 0:
+                    dwf.FDwfGetLastErrorMsg(szerr)
+                    print(str(szerr.value))
+                    quit()
+                if sts.value == 2:
+                    break
+
+            dwf.FDwfAnalogImpedanceStatusMeasure(hdwf, DwfAnalogImpedanceImpedance, byref(impedance))   # Read the DUT impedance value
+            dwf.FDwfAnalogImpedanceStatusMeasure(hdwf, DwfAnalogImpedanceImpedancePhase, byref(phase))  # Read the DUT phase value
+            extfile.write(str(freq) + "\t" + str(abs(impedance.value)) + "\t" + str((phase.value / math.pi) * 180.0) + "\n")    # Write frequency, impedance and phase value to file
+
+        extfile.close()
+
+    dwf.FDwfAnalogImpedanceConfigure(hdwf, c_int(0))    # Measurement End
+
+    final = time.time()                                 # Take time stamp
     infoOutput.insert(tk.INSERT, "Finished: " + str(round(final - initial, 2)) + "s\n")
-    infoOutput.see("end")
+    infoOutput.update()
+    infoOutput.see("end")                               # Allows scrolling in text widget
 
 
 def clearFunction():
+    clearButton.update()
     infoOutput.delete("1.0", END)
 
 
@@ -145,7 +163,7 @@ def quitFunction():
 # region Window Layout
 # This is the section of code which creates the main window
 w = 540  # width for the Tk root
-h = 300  # height for the Tk root
+h = 320  # height for the Tk root
 ws = win.winfo_screenwidth()  # width of the screen
 hs = win.winfo_screenheight()  # height of the screen
 x = (ws / 2) - (w)
@@ -157,58 +175,64 @@ win.title("Impedance Analyzer")
 
 
 # This is the section of code which creates all labels
-startLabel = Label(
-    win, text="Start Freq [Hz]", bg="#F0F8FF", font=("arial", 12, "normal")
-)
-startLabel.grid(row=1, column=1, sticky="W")
+startFreqLabel = Label(win, text="Start Freq [Hz]", bg="#F0F8FF", font=("arial", 12, "normal"))
+startFreqLabel.grid(row=1, column=1, sticky="W")
 
-endLabel = Label(win, text="End Freq [Hz]", bg="#F0F8FF", font=("arial", 12, "normal"))
-endLabel.grid(row=2, column=1, sticky="W")
+endFreqLabel = Label(win, text="End Freq [Hz]", bg="#F0F8FF", font=("arial", 12, "normal"))
+endFreqLabel.grid(row=2, column=1, sticky="W")
 
-deltaLabel = Label(
-    win, text="Delta Freq [Hz]", bg="#F0F8FF", font=("arial", 12, "normal")
-)
-deltaLabel.grid(row=3, column=1, sticky="W")
+deltaFreqLabel = Label(win, text="Delta Freq [Hz]", bg="#F0F8FF", font=("arial", 12, "normal"))
+deltaFreqLabel.grid(row=3, column=1, sticky="W")
 
-resistorLabel = Label(
-    win, text="Resistor [Ohm]", bg="#F0F8FF", font=("arial", 12, "normal")
-)
-resistorLabel.grid(row=4, column=1, sticky="W")
+startAmpLabel = Label(win, text="Start Amp [V]", bg="#F0F8FF", font=("arial", 12, "normal"))
+startAmpLabel.grid(row=4, column=1, sticky="W")
 
-amplitudeLabel = Label(
-    win, text="Amplitude [V]", bg="#F0F8FF", font=("arial", 12, "normal")
-)
-amplitudeLabel.grid(row=5, column=1, sticky="W")
+endAmpLabel = Label(win, text="End Amp [V]", bg="#F0F8FF", font=("arial", 12, "normal"))
+endAmpLabel.grid(row=5, column=1, sticky="W")
+
+deltaAmpLabel = Label(win, text="Delta Amp [V]", bg="#F0F8FF", font=("arial", 12, "normal"))
+deltaAmpLabel.grid(row=6, column=1, sticky="W")
+
+resistorLabel = Label(win, text="Resistor [Ohm]", bg="#F0F8FF", font=("arial", 12, "normal"))
+resistorLabel.grid(row=7, column=1, sticky="W")
 
 infoLabel = Label(win, text="Info", bg="#F0F8FF", font=("arial", 12, "normal"))
-infoLabel.grid(row=6, column=1, sticky="W")
+infoLabel.grid(row=8, column=1, sticky="W")
 
 
 # This is the section of code which creates all text input boxes
-startInput = Entry(win)
-startInput.insert(END, str(freq_start))
-startInput.grid(row=1, column=2, padx=20, ipadx=20, sticky="W")
+startFreqInput = Entry(win)
+startFreqInput.insert(END, str(freq_start))
+startFreqInput.grid(row=1, column=2, padx=20, ipadx=20, sticky="W")
 
-endInput = Entry(win)
-endInput.insert(END, str(freq_end))
-endInput.grid(row=2, column=2, padx=20, ipadx=20)
+endFreqInput = Entry(win)
+endFreqInput.insert(END, str(freq_end))
+endFreqInput.grid(row=2, column=2, padx=20, ipadx=20)
 
-deltaInput = Entry(win)
-deltaInput.insert(END, str(freq_delta))
-deltaInput.grid(row=3, column=2, padx=20, ipadx=20)
+deltaFreqInput = Entry(win)
+deltaFreqInput.insert(END, str(freq_delta))
+deltaFreqInput.grid(row=3, column=2, padx=20, ipadx=20)
+
+startAmpInput = Entry(win)
+startAmpInput.insert(END, str(amp_start))
+startAmpInput.grid(row=4, column=2, padx=20, ipadx=20)
+
+endAmpInput = Entry(win)
+endAmpInput.insert(END, str(amp_end))
+endAmpInput.grid(row=5, column=2, padx=20, ipadx=20)
+
+deltaAmpInput = Entry(win)
+deltaAmpInput.insert(END, str(amp_delta))
+deltaAmpInput.grid(row=6, column=2, padx=20, ipadx=20)
 
 resistanceInput = Entry(win)
 resistanceInput.insert(END, str(resistance))
-resistanceInput.grid(row=4, column=2, padx=20, ipadx=20)
-
-amplitudeInput = Entry(win)
-amplitudeInput.insert(END, str(amplitude))
-amplitudeInput.grid(row=5, column=2, padx=20, ipadx=20)
+resistanceInput.grid(row=7, column=2, padx=20, ipadx=20)
 
 
 # This is the section of code which creates all text output boxes
 infoOutput = Text(win, height=5, width=25)
-infoOutput.grid(row=6, column=2, sticky="W")
+infoOutput.grid(row=8, column=2, sticky="W")
 
 
 # This is the section of code which creates all buttons
@@ -218,9 +242,7 @@ connectButton = Button(
     state=NORMAL,
     bg="#F0F8FF",
     font=("arial", 12, "normal"),
-    command=connectFunction,
-)
-# connectButton = ttk.Button(win, text = 'Connect')
+    command=connectFunction,)
 connectButton.grid(row=1, column=3, padx=40, sticky="E", ipadx=25)
 
 disconnectButton = Button(
@@ -229,8 +251,7 @@ disconnectButton = Button(
     state=DISABLED,
     bg="#F0F8FF",
     font=("arial", 12, "normal"),
-    command=disconnectFunction,
-)
+    command=disconnectFunction,)
 disconnectButton.grid(row=2, column=3, padx=40, sticky="E", ipadx=25)
 
 setButton = Button(
@@ -239,8 +260,7 @@ setButton = Button(
     state=DISABLED,
     bg="#F0F8FF",
     font=("arial", 12, "normal"),
-    command=setFunction,
-)
+    command=setFunction,)
 setButton.grid(row=4, column=3, padx=40, sticky="E", ipadx=25)
 
 startButton = Button(
@@ -249,8 +269,7 @@ startButton = Button(
     state=DISABLED,
     bg="#F0F8FF",
     font=("arial", 12, "normal"),
-    command=startFunction,
-)
+    command=startFunction,)
 startButton.grid(row=5, column=3, padx=40, sticky="E", ipadx=25)
 
 clearButton = Button(
@@ -259,8 +278,7 @@ clearButton = Button(
     state=NORMAL,
     bg="#F0F8FF",
     font=("arial", 12, "normal"),
-    command=clearFunction,
-)
+    command=clearFunction,)
 clearButton.grid(row=6, column=3, padx=40, sticky="E", ipadx=25)
 
 quitButton = Button(
@@ -269,8 +287,7 @@ quitButton = Button(
     state=NORMAL,
     bg="#F0F8FF",
     font=("arial", 12, "normal"),
-    command=quitFunction,
-)
+    command=quitFunction,)
 quitButton.grid(row=7, column=3, padx=40, sticky="E", ipadx=25)
 # endregion
 
