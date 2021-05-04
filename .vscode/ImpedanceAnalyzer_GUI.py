e2b4dd0b9336b8c21ca66cd88ddd3d232a8d2477
lindnerlars <lindnerlars81@gmail.com>
2021-05-04 13:49:15 -0700

    04/05/21

--- .vscode/ImpedanceAnalyzer_GUI.py
+++ .vscode/ImpedanceAnalyzer_GUI.py
@@ -5,6 +5,7 @@
 
    -> This programm does a linear frequency sweep from 'freq_start' to 'freq_end' with 'freq_delta' using a GUI interface
    -> It prints the numerical values of frequency [Hz], impedance [Ohm] and phase [deg] as a txt-file
+   -> Das ist nur ein Test, um GitHub su testen
 """
 
 
